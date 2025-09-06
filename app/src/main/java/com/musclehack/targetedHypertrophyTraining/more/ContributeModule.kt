52eb3026e4cc19c484188b27e0644313fd198078
Juan Taveras IV <juanito.taveras@gmail.com>
2025-09-06 11:39:58 -0700

    Added build.gradle and extracted signing keys into separate file.

--- app/src/main/java/com/musclehack/targetedHypertrophyTraining/more/ContributeModule.kt
+++ app/src/main/java/com/musclehack/targetedHypertrophyTraining/more/ContributeModule.kt
@@ -1,12 +1,8 @@
 package com.musclehack.targetedHypertrophyTraining.more
 
-import androidx.lifecycle.ViewModel
 import com.musclehack.targetedHypertrophyTraining.core.dependencyInjection.ViewModelBuilder
-import com.musclehack.targetedHypertrophyTraining.core.dependencyInjection.ViewModelKey
-import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import dagger.multibindings.IntoMap
 
 @Module
 abstract class ContributeModule {
